5c6aa90daaa72c52f22840d469b0baf1eb5a9c09
rahulbhate <bhate.rahul@gmail.com>
2019-12-03 11:32:23 +1100

    mongo db connection string changed

--- db_connection.js
+++ db_connection.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config();
+//mongodb://admin:<password>@cluster0-shard-00-00-pbkmp.mongodb.net:27017,cluster0-shard-00-01-pbkmp.mongodb.net:27017,cluster0-shard-00-02-pbkmp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority
 mongoose
   .connect(
-    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-pbkmp.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`,
+    `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-shard-00-00-pbkmp.mongodb.net:27017,cluster0-shard-00-01-pbkmp.mongodb.net:27017,cluster0-shard-00-02-pbkmp.mongodb.net:27017/${process.env.MONGO_DB}?replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority`,
     { useNewUrlParser: true, useUnifiedTopology: true  },
   )
   .then((result) => {
