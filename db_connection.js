245999ecfe87c3f0594191f2a2c2325d117f9b91
rahulbhate <bhate.rahul@gmail.com>
2019-12-03 14:40:57 +1100

    database connection

--- db_connection.js
+++ db_connection.js
@@ -4,7 +4,7 @@ dotenv.config();
 //mongodb://admin:<password>@cluster0-shard-00-00-pbkmp.mongodb.net:27017,cluster0-shard-00-01-pbkmp.mongodb.net:27017,cluster0-shard-00-02-pbkmp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority
 mongoose
   .connect(
-    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-pbkmp.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`,
+    `process.env.MONGOLAB_URI`,
     { useNewUrlParser: true, useUnifiedTopology: true  },
   )
   .then((result) => {
